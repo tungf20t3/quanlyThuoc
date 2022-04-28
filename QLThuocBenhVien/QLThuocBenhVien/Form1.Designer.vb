02ac7b04ebb113a95e9c62b84a9e08f481a7749d
tungf20t3 <20t3.luuvantung@gmail.com>
2022-04-28 22:09:57 +0700

    quan li thuoc

---
+++ QLThuocBenhVien/QLThuocBenhVien/Form1.Designer.vb
@@ -0,0 +1,344 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.btnXoa = New System.Windows.Forms.Button()
+        Me.btnSua = New System.Windows.Forms.Button()
+        Me.btnThem = New System.Windows.Forms.Button()
+        Me.tb6 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.tb5 = New System.Windows.Forms.TextBox()
+        Me.tb3 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.tb4 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.tb2 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.tb1 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lv = New System.Windows.Forms.ListView()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage2.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.TabControl1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.btnXoa)
+        Me.TabPage2.Controls.Add(Me.btnSua)
+        Me.TabPage2.Controls.Add(Me.btnThem)
+        Me.TabPage2.Controls.Add(Me.tb6)
+        Me.TabPage2.Controls.Add(Me.Label5)
+        Me.TabPage2.Controls.Add(Me.tb5)
+        Me.TabPage2.Controls.Add(Me.tb3)
+        Me.TabPage2.Controls.Add(Me.Label8)
+        Me.TabPage2.Controls.Add(Me.Label7)
+        Me.TabPage2.Controls.Add(Me.tb4)
+        Me.TabPage2.Controls.Add(Me.Label6)
+        Me.TabPage2.Controls.Add(Me.tb2)
+        Me.TabPage2.Controls.Add(Me.Label4)
+        Me.TabPage2.Controls.Add(Me.tb1)
+        Me.TabPage2.Controls.Add(Me.TextBox1)
+        Me.TabPage2.Controls.Add(Me.Label3)
+        Me.TabPage2.Controls.Add(Me.Label2)
+        Me.TabPage2.Controls.Add(Me.lv)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 37)
+        Me.TabPage2.Margin = New System.Windows.Forms.Padding(4)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(4)
+        Me.TabPage2.Size = New System.Drawing.Size(1273, 626)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Quản lý Thuốc"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'btnXoa
+        '
+        Me.btnXoa.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnXoa.Location = New System.Drawing.Point(1084, 474)
+        Me.btnXoa.Name = "btnXoa"
+        Me.btnXoa.Size = New System.Drawing.Size(107, 48)
+        Me.btnXoa.TabIndex = 15
+        Me.btnXoa.Text = "Xóa"
+        Me.btnXoa.UseVisualStyleBackColor = True
+        '
+        'btnSua
+        '
+        Me.btnSua.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnSua.ForeColor = System.Drawing.Color.Crimson
+        Me.btnSua.Location = New System.Drawing.Point(927, 474)
+        Me.btnSua.Name = "btnSua"
+        Me.btnSua.Size = New System.Drawing.Size(107, 48)
+        Me.btnSua.TabIndex = 14
+        Me.btnSua.Text = "Sửa"
+        Me.btnSua.UseVisualStyleBackColor = True
+        '
+        'btnThem
+        '
+        Me.btnThem.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnThem.ForeColor = System.Drawing.Color.SeaGreen
+        Me.btnThem.Location = New System.Drawing.Point(757, 474)
+        Me.btnThem.Name = "btnThem"
+        Me.btnThem.Size = New System.Drawing.Size(107, 48)
+        Me.btnThem.TabIndex = 13
+        Me.btnThem.Text = "Thêm"
+        Me.btnThem.UseVisualStyleBackColor = True
+        '
+        'tb6
+        '
+        Me.tb6.Location = New System.Drawing.Point(911, 385)
+        Me.tb6.Name = "tb6"
+        Me.tb6.Size = New System.Drawing.Size(264, 34)
+        Me.tb6.TabIndex = 12
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(789, 391)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(96, 28)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Số lượng:"
+        '
+        'tb5
+        '
+        Me.tb5.Location = New System.Drawing.Point(911, 324)
+        Me.tb5.Name = "tb5"
+        Me.tb5.Size = New System.Drawing.Size(264, 34)
+        Me.tb5.TabIndex = 10
+        '
+        'tb3
+        '
+        Me.tb3.Location = New System.Drawing.Point(911, 206)
+        Me.tb3.Name = "tb3"
+        Me.tb3.Size = New System.Drawing.Size(264, 34)
+        Me.tb3.TabIndex = 10
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(748, 330)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(136, 28)
+        Me.Label8.TabIndex = 9
+        Me.Label8.Text = "Nhà cung cấp:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(785, 212)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(100, 28)
+        Me.Label7.TabIndex = 9
+        Me.Label7.Text = "Tên thuốc:"
+        '
+        'tb4
+        '
+        Me.tb4.Location = New System.Drawing.Point(911, 269)
+        Me.tb4.Name = "tb4"
+        Me.tb4.Size = New System.Drawing.Size(264, 34)
+        Me.tb4.TabIndex = 8
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(757, 275)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(127, 28)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "Hạn sử dụng:"
+        '
+        'tb2
+        '
+        Me.tb2.Location = New System.Drawing.Point(911, 146)
+        Me.tb2.Name = "tb2"
+        Me.tb2.Size = New System.Drawing.Size(264, 34)
+        Me.tb2.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(777, 152)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(107, 28)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Loại thuốc:"
+        '
+        'tb1
+        '
+        Me.tb1.Location = New System.Drawing.Point(911, 85)
+        Me.tb1.Name = "tb1"
+        Me.tb1.Size = New System.Drawing.Size(264, 34)
+        Me.tb1.TabIndex = 2
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(911, 85)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(185, 34)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(785, 91)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(99, 28)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Mã thuốc:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(785, 91)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 28)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label2"
+        '
+        'lv
+        '
+        Me.lv.FullRowSelect = True
+        Me.lv.HideSelection = False
+        Me.lv.Location = New System.Drawing.Point(23, 51)
+        Me.lv.Name = "lv"
+        Me.lv.Size = New System.Drawing.Size(710, 530)
+        Me.lv.TabIndex = 0
+        Me.lv.UseCompatibleStateImageBehavior = False
+        Me.lv.View = System.Windows.Forms.View.Details
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.PictureBox1)
+        Me.TabPage1.Controls.Add(Me.Panel1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 37)
+        Me.TabPage1.Margin = New System.Windows.Forms.Padding(4)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(4)
+        Me.TabPage1.Size = New System.Drawing.Size(1273, 626)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Trang chủ"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.ErrorImage = CType(resources.GetObject("PictureBox1.ErrorImage"), System.Drawing.Image)
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.InitialImage = CType(resources.GetObject("PictureBox1.InitialImage"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(406, 154)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(379, 349)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.SeaGreen
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1227, 148)
+        Me.Panel1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.SystemColors.Window
+        Me.Label1.Location = New System.Drawing.Point(366, 58)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(503, 38)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "QUẢN LÝ THUỐC TRONG BỆNH VIỆN"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(13, 1)
+        Me.TabControl1.Margin = New System.Windows.Forms.Padding(4)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(1281, 667)
+        Me.TabControl1.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 28.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1244, 681)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Quản lý thuốc bệnh viện"
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        Me.TabPage1.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.TabControl1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents tb5 As TextBox
+    Friend WithEvents tb3 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents tb4 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents tb2 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents tb1 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lv As ListView
+    Friend WithEvents btnXoa As Button
+    Friend WithEvents btnSua As Button
+    Friend WithEvents btnThem As Button
+    Friend WithEvents tb6 As TextBox
+    Friend WithEvents Label5 As Label
+End Class
---
+++ QLThuocBenhVien/QLThuocBenhVien/Form1.vb
@@ -0,0 +1,69 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lv.Columns.Add("Mã thuốc", 60, HorizontalAlignment.Center)
+        lv.Columns.Add("Loại thuốc", 130, HorizontalAlignment.Center)
+        lv.Columns.Add("Tên thuốc", 130, HorizontalAlignment.Center)
+        lv.Columns.Add("Hạn sử dụng", 130, HorizontalAlignment.Center)
+        lv.Columns.Add("Nhà cung cấp", 130, HorizontalAlignment.Center)
+        lv.Columns.Add("Số lượng", 130, HorizontalAlignment.Center)
+    End Sub
+
+    Private Sub lv_Click(sender As Object, e As EventArgs) Handles lv.Click
+        If lv.SelectedItems.Count > 0 Then
+            tb1.Text = lv.SelectedItems(0).SubItems(0).Text
+            tb2.Text = lv.SelectedItems(0).SubItems(1).Text
+            tb3.Text = lv.SelectedItems(0).SubItems(2).Text
+            tb4.Text = lv.SelectedItems(0).SubItems(3).Text
+            tb5.Text = lv.SelectedItems(0).SubItems(4).Text
+            tb6.Text = lv.SelectedItems(0).SubItems(5).Text
+        End If
+    End Sub
+
+    Private Sub btnThem_Click(sender As Object, e As EventArgs) Handles btnThem.Click
+        Dim ptu As New ListViewItem(tb1.Text)
+        ptu.SubItems.Add(tb2.Text)
+        ptu.SubItems.Add(tb3.Text)
+        ptu.SubItems.Add(tb4.Text)
+        ptu.SubItems.Add(tb5.Text)
+        ptu.SubItems.Add(tb6.Text)
+
+        lv.Items.Add(ptu)
+
+        tb1.Text = ""
+        tb2.Text = ""
+        tb3.Text = ""
+        tb4.Text = ""
+        tb5.Text = ""
+        tb6.Text = ""
+    End Sub
+
+    Private Sub btnSua_Click(sender As Object, e As EventArgs) Handles btnSua.Click
+        If lv.SelectedItems.Count > 0 Then
+            lv.SelectedItems(0).SubItems(0).Text = tb1.Text
+            lv.SelectedItems(0).SubItems(1).Text = tb2.Text
+            lv.SelectedItems(0).SubItems(2).Text = tb3.Text
+            lv.SelectedItems(0).SubItems(3).Text = tb4.Text
+            lv.SelectedItems(0).SubItems(4).Text = tb5.Text
+            lv.SelectedItems(0).SubItems(5).Text = tb6.Text
+
+            tb1.Text = ""
+            tb2.Text = ""
+            tb3.Text = ""
+            tb4.Text = ""
+            tb5.Text = ""
+            tb6.Text = ""
+        End If
+    End Sub
+
+    Private Sub btnXoa_Click(sender As Object, e As EventArgs) Handles btnXoa.Click
+        If lv.SelectedItems.Count > 0 Then
+            lv.Items.Remove(lv.SelectedItems(0))
+            tb1.Text = ""
+            tb2.Text = ""
+            tb3.Text = ""
+            tb4.Text = ""
+            tb5.Text = ""
+            tb6.Text = ""
+        End If
+    End Sub
+End Class
